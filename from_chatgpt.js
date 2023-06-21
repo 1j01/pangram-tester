277b6871dc0636164914d6b70ca301f3a5eccaf8
Isaiah Odhner <isaiahodhner@gmail.com>
2023-06-20 22:25:51 -0400

    This is terrible

--- from_chatgpt.js
+++ from_chatgpt.js
@@ -12,6 +12,46 @@ const codeTargetFile = 'index.html';
 const codeTargetLanguage = 'html'; // .language-<codeTargetLanguage> class
 const commitMessageFile = 'commit_message.txt';
 
+function markMessageContentElements() {
+  // Unfortunately, OpenAI uses only presentational CSS classes.
+  // Use the largest font size to guess which element contains the message content.
+  // This will fail if there is a heading in the message.
+  // Also, how will this distinguish between inner and outer elements which may share a font size?
+  // TODO: find a new strategy for identifying message content elements
+
+  function findElementWithLargestFontSize(element) {
+    let largestFontSize = -Infinity;
+    let elementWithLargestFontSize = null;
+  
+    const traverseDescendants = (currentElement) => {
+      const children = currentElement.children;
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        const computedStyle = window.getComputedStyle(child);
+        const fontSize = parseFloat(computedStyle.fontSize);
+  
+        if (fontSize > largestFontSize) {
+          largestFontSize = fontSize;
+          elementWithLargestFontSize = child;
+        }
+  
+        traverseDescendants(child); // Recursively traverse nested elements
+      }
+    };
+  
+    traverseDescendants(element);
+  
+    return elementWithLargestFontSize;
+  }
+  
+  // TODO: see if .group contains other messages in non-linear conversations
+  const messageElements = document.querySelectorAll('.group');
+  for (const messageElement of messageElements) {
+    const contentElement = findElementWithLargestFontSize(messageElement);
+    contentElement.classList.add('best-guess-for-message-content');
+  }
+}
+
 async function exportChatHistory() {
   // Export the HTML content of the ChatGPT page to a file
   // This step could be cached, but it's fast enough to just do it every time
@@ -34,17 +74,17 @@ async function commitCodeBlocks() {
   const html = await fs.readFile(conversationFile, 'utf-8');
   const $ = cheerio.load(html);
   const $codeBlocks = $(`code.language-${codeTargetLanguage}`);
-  const $messageBodies = $(".prose");
-  console.log(`Found ${$codeBlocks.length} ${codeTargetLanguage} code blocks, ${$messageBodies.length} messages`);
+  const $messages = $(".group");
+  console.log(`Found ${$codeBlocks.length} ${codeTargetLanguage} code blocks, ${$messages.length} messages`);
   // Commit each code block with its surrounding text as the commit message
   for (const codeElement of $codeBlocks) {
     const code = $(codeElement).text();
-    const $message = $(codeElement).closest('.prose');
+    const $message = $(codeElement).closest('.group');
     // elide the code block (and outer pre element, which includes a copy button) from the commit message
     const $placeholder = $("<span></span>").text("\n\n<code>\n\n");
     $(codeElement).closest('pre').replaceWith($placeholder);
     // const $previousMessage = $message.closest(".group").prev().find('.prose');
-    const messageIndex = $messageBodies.index($message[0]);
+    const messageIndex = $messages.index($message[0]);
     if (messageIndex === 0) {
       console.error('WARNING: Skipping first message. Note that code sent as part of a prompt is not handled correctly yet. It may be skipped (like this) or mislabeled as a ChatGPT response.');
       continue;
@@ -53,7 +93,7 @@ async function commitCodeBlocks() {
       console.error('ERROR: Could not find message index');
       continue;
     }
-    const $previousMessage = $($messageBodies[messageIndex - 1]);
+    const $previousMessage = $($messages[messageIndex - 1]);
     const prompt = $previousMessage.text().trim();
     const responseMinusCode = $message.text().trim();
     const message = `Commit with ChatGPT\n\nChatGPT prompt:\n${formatQuote(prompt)}\n\nChatGPT response:\n${formatQuote(responseMinusCode)}`;
